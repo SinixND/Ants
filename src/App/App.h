8f4475038644cd4f91158b79fc46138fe562d729
SinixND <schaumberger_andreas@gmx.de>
2025-07-09 22:04:32 +0200

    init

---
+++ src/App/App.h
@@ -0,0 +1,35 @@
+#ifndef IG20240519210141
+#define IG20240519210141
+
+#include "AppConfigs.h"
+#include "Simulation.h"
+#include <raylib.h>
+
+struct AppConfig;
+
+/// Handles windows, rendering etc.
+class App
+{
+    /// Possible file export
+    AppConfig config{};
+
+public:
+    Simulation simulation{};
+
+    int screenWidth{};
+    int screenHeight{};
+    Vector2 mousePosition{};
+    float dt{ 0 };
+
+public:
+    void init();
+    /// Main app loop
+    void run();
+    void render();
+    void deinit();
+
+private:
+    void setupAppEvents();
+};
+
+#endif
